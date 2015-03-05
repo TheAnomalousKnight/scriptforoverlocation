da54909097ccd4ce6e52ee8112c4111499de3ed2
TheAnomalousKnight <smartraghav_raghav@yahoo.com>
2015-03-05 19:53:20 +0530

    Create scriptforoverlocation.js

---
+++ scriptforoverlocation.js
@@ -0,0 +1,11 @@
+function overone() {
+   document.getElementById("location").src = "http://1.bp.blogspot.com/-WptyJe8_JTY/VPbvtvHObZI/AAAAAAAAASE/s2vEBoOYbdI/s1600/locrender2.png"
+}
+
+function outtwo() {
+   document.getElementById("location").src = "http://1.bp.blogspot.com/-XZ_3qDci6L0/VPbvtrqLuDI/AAAAAAAAASA/6w2dsS7LRtw/s1600/locrender1.png"
+}
+
+function clickthree() {
+   document.getElementById("location").src = "http://1.bp.blogspot.com/-Ald8bkTz_0o/VPbvuTVbnzI/AAAAAAAAASQ/YJvROReb9D4/s1600/locrender3.png"
+}
